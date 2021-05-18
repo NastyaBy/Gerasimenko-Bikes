2900dbd4152e06dbfa3961dc10515ca76fcd3acb
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-05-18 13:43:18 +0300

    валидация полей импут

---
+++ source/js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const inputName = document.querySelector('.js-validateName');
+const inputTel = document.querySelector('.js-validateTel');
+const form = document.querySelector('.js-form');
+const sendForm = document.querySelector('.js-sendForm');
+
+const inputHandlerTel = (e) => {
+  const el = e.target;
+  const pattern = /\D/g;
+
+  el.value = el.value.replace(pattern, '');
+};
+
+const inputHandlerName = (e) => {
+  const el = e.target;
+  const pattern = /\d/g;
+
+  el.value = el.value.replace(pattern, '');
+};
+
+inputTel.addEventListener('input', inputHandlerTel);
+inputName.addEventListener('input', inputHandlerName);
+
+sendForm.addEventListener('click', () => {
+  const obj = {
+    name: inputName.value,
+    tel: inputTel.value,
+  };
+  const serialObj = JSON.stringify(obj);
+  localStorage.setItem('order', serialObj);
+});
