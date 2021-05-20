026d9f9489f6430e46a3a4dc9289f7f872efc306
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-05-20 18:18:04 +0300

    внесение правок

--- source/js/main.js
+++ source/js/main.js
@@ -1,52 +1,61 @@
 'use strict';
 
-const layout = document.querySelector('.layout');
-const menuToggle = document.querySelector('.js-menuToggle');
-const menuPopup = document.querySelector('.js-menuPopup');
-const navigations = document.querySelectorAll('.js-navigation');
-
-const inputName = document.querySelector('.js-validateName');
-const inputTel = document.querySelector('.js-validateTel');
-const form = document.querySelector('.js-form');
-const sendForm = document.querySelector('.js-sendForm');
+var layout = document.querySelector('.layout');
+var menuToggle = document.querySelector('.js-menuToggle');
+var menuPopup = document.querySelector('.js-menuPopup');
+var navigations = document.querySelectorAll('.js-navigation');
+var inputName = document.querySelector('.js-validateName');
+var inputTel = document.querySelector('.js-validateTel');
+var sendForm = document.querySelector('.js-sendForm');
 
 layout.classList.remove('no-js');
 
-menuToggle.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  menuToggle.classList.toggle('header-toggle--active');
-  menuPopup.classList.toggle('header-nav--active');
-});
+if (menuToggle) {
+  menuToggle.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    menuToggle.classList.toggle('header-toggle--active');
+    menuPopup.classList.toggle('header-nav--active');
+  });
+}
 
-navigations.forEach(trigger => (
-  trigger.addEventListener('click', () => {
-    menuToggle.classList.remove('header-toggle--active');
-    menuPopup.classList.remove('header-nav--active');
-  })
-));
+if (navigations) {
+  navigations.forEach(function (trigger) {
+    trigger.addEventListener('click', function () {
+      menuToggle.classList.remove('header-toggle--active');
+      menuPopup.classList.remove('header-nav--active');
+    });
+  });
+}
 
-const inputHandlerTel = (e) => {
-  const el = e.target;
-  const pattern = /\D/g;
-
-  el.value = el.value.replace(pattern, '');
-};
-
-const inputHandlerName = (e) => {
-  const el = e.target;
-  const pattern = /\d/g;
+function inputHandlerTel(e) {
+  var el = e.target;
+  var pattern = /\D/g;
 
   el.value = el.value.replace(pattern, '');
-};
+}
 
-inputTel.addEventListener('input', inputHandlerTel);
-inputName.addEventListener('input', inputHandlerName);
+function inputHandlerName(e) {
+  var el = e.target;
+  var pattern = /\d/g;
 
-sendForm.addEventListener('click', () => {
-  const obj = {
-    name: inputName.value,
-    tel: inputTel.value,
-  };
-  const serialObj = JSON.stringify(obj);
-  localStorage.setItem('order', serialObj);
-});
+  el.value = el.value.replace(pattern, '');
+}
+
+if (inputTel) {
+  inputTel.addEventListener('input', inputHandlerTel);
+}
+
+if (inputName) {
+  inputName.addEventListener('input', inputHandlerName);
+}
+
+if (sendForm) {
+  sendForm.addEventListener('click', function () {
+    var obj = {
+      name: inputName.value,
+      tel: inputTel.value,
+    };
+    var serialObj = JSON.stringify(obj);
+    localStorage.setItem('order', serialObj);
+  });
+}
